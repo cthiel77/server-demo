e2c3e76a5260c43ab3fe5b394d64ce004fc9ecf3
Carsten Thiel <dev@thiel-inet.de>
2023-11-16 16:56:14 +0100

    feat: add error response struct and example data (#13)

--- internal/db/datasets.go
+++ internal/db/datasets.go
@@ -2,8 +2,8 @@ package db
 
 // HeroSet a country dataset to be stored
 type HeroSet struct {
-	ID        uint64 `example:"123"`
-	FirstName string `example:"Wade"`
-	LastName  string `example:"Wilson"`
-	HeroName  string `example:"Deadpool"`
+	ID        uint64 `json:"id" example:"123"`
+	FirstName string `json:"firstName" example:"Wade"`
+	LastName  string `json:"lastName" example:"Wilson"`
+	HeroName  string `json:"heroName" example:"Deadpool"`
 }
