fb4918e533a450213934f14b380e74ff8c65b7e8
Carsten Thiel <dev@thiel-inet.de>
2023-11-17 22:51:22 +0100

    task: added app mode dependent template contents

--- internal/template.go
+++ internal/template.go
@@ -13,6 +13,7 @@ func RenderTemplate(tplContent string, data any) (s string, err *defaultError.Er
 	t, e := template.New("page_content").Parse(tplContent)
 	if e != nil {
 		err = defaultError.FromGoError("1700227900", defaultError.StatusNotAcceptableData, e)
+		s = `<i class"icon icon-ban-circle"></i> An Error accured, while rendering a template. Look into log outpout for more details`
 		return
 	}
 	t.Execute(&buf, data)
