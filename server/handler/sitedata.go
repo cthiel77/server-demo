2c587def2cbc7dacaf869d41319ac6cf57a4a939
Carsten Thiel <dev@thiel-inet.de>
2023-11-18 09:02:43 +0100

    task: added app info

--- server/handler/sitedata.go
+++ server/handler/sitedata.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"github.com/cthiel77/server-demo/internal/cfg"
+	"github.com/cthiel77/server-demo/internal/log"
 	"github.com/cthiel77/server-demo/internal/meta"
 	"github.com/gofiber/fiber/v2"
 )
@@ -12,6 +14,9 @@ func initSiteData(currentPageKey string) (data fiber.Map) {
 	data["company"] = meta.GetAuthorCompany()
 	data["page_menu"] = getNavigationData()
 	data["current_page"] = currentPageKey
-
+	data["version"] = meta.GetVersionID()
+	data["buildTime"] = meta.GetBuildTime()
+	data["appMode"] = cfg.GetAppModeKey()
+	data["logLevel"] = log.GetLogLevel()
 	return
 }
